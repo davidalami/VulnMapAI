937d1f8ae8eba48a3c15a0a9534a4d3f8305e9f3
David Alami <davidalami@gmail.com>
2023-09-30 13:08:08 +0300

    Factor out common ports loading logic into a separate function

--- recon/async_scanner.py
+++ recon/async_scanner.py
@@ -15,11 +15,15 @@ class NetworkScanner:
 
     def __init__(self, target_subnets: List[str], top_ports=300):
         self.target_subnets = target_subnets
-        with open(COMMON_PORTS) as f:
-            self.ports = json.load(f)
         self.top_ports = top_ports
+        self.ports = NetworkScanner._load_common_ports(COMMON_PORTS)
         self.results_queue = multiprocessing.Queue()
         self.logger = Logger(__name__).get_logger()
+
+    @staticmethod
+    def _load_common_ports(common_ports_file: str) -> List[str]:
+        with open(common_ports_file) as f:
+            return json.load(f)
 
     def extract_port_info(self, scanned_host: str, protocol_type: str, port_number: int, scan_data: Dict) -> Dict:
         """
