2bd4cbc2f13fb570029560b2f85881425c8522e8
David Alami <davidalami@gmail.com>
2023-10-02 18:13:09 +0300

    Integrate webapp to main

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import argparse
+
 from orchestration.pentest import PenetrationTester
+from webapp.app import app
 
 
 def main(targets, top_ports):
     pentester = PenetrationTester(targets, top_ports)
     pentester.scan_and_report()
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
 if __name__ == "__main__":
