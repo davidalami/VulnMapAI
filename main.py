6530f99f928d9ae9b7fbfb05f5ebc583fe641a3a
David Alami <davidalami@gmail.com>
2023-10-02 21:21:02 +0300

    Disable debug mode in flask to prevent double scanning

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from webapp.app import app
 def main(targets, top_ports):
     pentester = PenetrationTester(targets, top_ports)
     pentester.scan_and_report()
-    app.run(debug=True, host='0.0.0.0', port=1337)
+    app.run(debug=False, host='0.0.0.0', port=1337)
 
 
 if __name__ == "__main__":
