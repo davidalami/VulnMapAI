e2be3b2bc7d98800c93f4de7e446e61af6373aa4
David Alami <davidalami@gmail.com>
2023-10-02 21:18:18 +0300

    Improve HTML formatting of the report index page

--- webapp/app.py
+++ webapp/app.py
@@ -2,7 +2,8 @@ import os
 
 from flask import (
     Flask,
-    send_from_directory
+    render_template_string,
+    send_from_directory,
 )
 
 from config import REPORT_DIR
@@ -16,8 +17,32 @@ app.config['REPORT_FOLDER'] = str(REPORT_DIR.absolute())
 @app.route('/')
 def index():
     files = [f for f in os.listdir(app.config['REPORT_FOLDER']) if f.endswith('.html')]
-    links = [f"<a href='/report/{f}'>{f}</a>" for f in files]
-    return '<br>'.join(links)
+
+    html = '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Report Index</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <h4 class="center-align">Report Index</h4>
+            <ul class="collection">
+                {% for file in files %}
+                    <li class="collection-item"><a href="/report/{{ file }}">{{ file }}</a></li>
+                {% endfor %}
+            </ul>
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </body>
+    </html>
+    '''
+
+    return render_template_string(html, files=files)
+
 
 
 @app.route('/report/<filename>')
