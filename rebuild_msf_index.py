f4a8fb2c5efba19e178b2ce8576c4132211a2083
David Alami <david.alami@gmail.com>
2023-10-06 18:14:54 +0300

    Update docstring for build_index function

--- rebuild_msf_index.py
+++ rebuild_msf_index.py
@@ -10,17 +10,17 @@ import json
 from config import EXPLOITS_DIR, MSF_INDEX_FILE
 
 
-def build_index(exploits_dir=EXPLOITS_DIR):
+def build_index(exploits_dir=EXPLOITS_DIR) -> None:
     """
     Parses and indexes the Name and Description from Ruby (.rb) exploit files located in the specified directory.
-    
-    Parameters:
-        - exploits_dir (str): The path to the directory containing the Ruby exploit files. Defaults to `EXPLOITS_DIR` from config.
-
     Saves the indexed data as a JSON file (`MSF_INDEX_FILE`).
-
     The indexing will go through each Ruby file, extract the exploit's name and description, and add this information 
     to the index, along with the relative path to the exploit.
+    
+    Args:
+        exploits_dir (str): The path to the directory containing the Ruby exploit files. Defaults to `EXPLOITS_DIR` from config.
+    Returns:
+        None
     """
     index = []
     name_pattern = re.compile(r"'Name'\s*=>\s*'([^']*)'", re.IGNORECASE)
