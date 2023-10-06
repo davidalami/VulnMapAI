0287d23c81e357dcdd65a9d83f2d110f1d9678af
David Alami <david.alami@gmail.com>
2023-10-06 18:10:18 +0300

    Write a module-level docstring for rebuild_msf_index.py

--- rebuild_msf_index.py
+++ rebuild_msf_index.py
@@ -1,3 +1,8 @@
+"""
+This module provides functionality to build an index of exploits found in the specified directory.
+It will search for Ruby files, parse the exploit's name and description, and save the data to a JSON index file.
+"""
+
 import os
 import re
 import json
