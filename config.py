63da52a175721d5b236c306291c21b7a6b8c7817
David Alami <davidalami@gmail.com>
2023-10-10 09:21:54 +0300

    Add prompts file to config

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ MSF_INDEX_FILE = '/app/resources/msf_index.json'
 COMMON_PORTS = "/app/resources/ports.json"
 REPORT_DIR = Path('./report')
 REPORT_TEMPLATE = '/app/webapp/templates/report_template.html'
+PROMPTS_FILE = "/app/advising/prompts.yaml"
